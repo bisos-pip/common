eb3565ac1616060e221d72d57def15b2d98b7dbc
Mohsen-Bisp0017 BANAN <form@mohsen.banan.1.byname.net>
2018-12-01 22:06:19 -0800

    Added mohsenBanan repos.

--- dev/bisos/common/baseDirBxRepos.py
+++ dev/bisos/common/baseDirBxRepos.py
@@ -831,6 +831,10 @@ def pbdDict_bxReposRoot(
     # So, gitCloneBase should be renamed gitReposCollectionBase and gitRepoBase
     #
 
+    directory( 'mohsenBanan')
+    gitClone( 'mohsenBanan/ReposOverview',  'mohsenBanan/ReposOverview', vcMode)
+    gitClone( 'mohsenBanan/StartHere',  'mohsenBanan/StartHere', vcMode)        
+
     gitCloneBase( 'ByStar',  'ByStar/base', vcMode)    
     gitClone( 'ByStar/overview',  'ByStar/overview', vcMode)
 
