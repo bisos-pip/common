302fb46f852d3faa39ce84b4752fe82e28eaaedf
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-02-24 22:59:17 -0800

    0.92 added bisos-pip/sites.

--- py3/bisos/common/baseDirBxRepos.py
+++ py3/bisos/common/baseDirBxRepos.py
@@ -1054,6 +1054,7 @@ def pbdDict_bxReposRoot(
     gitClone('bisos-pip/io',  'bisos-pip/io', vcMode)
     gitClone('bisos-pip/qmail',  'bisos-pip/qmail', vcMode)
     gitClone('bisos-pip/regfps', 'bisos-pip/regfps', vcMode)
+    gitClone('bisos-pip/sites', 'bisos-pip/sites', vcMode)
     gitClone('bisos-pip/siteRegistrars', 'bisos-pip/siteRegistrars', vcMode)
     gitClone('bisos-pip/banna', 'bisos-pip/banna', vcMode)
     # gitClone('bisos-pip/sites', 'bisos-pip/sites', vcMode)
